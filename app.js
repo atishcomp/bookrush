2c688afa6551ac05f2e615e71976db669576207a
ATISH YADAV <atishcomp@gmail.com>
2022-12-30 13:08:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express =require("express");
+require('dotenv').config();
+var app= express();
+const mongoose = require("mongoose");
+var cors = require("cors");
+var bodyParser= require("body-parser");
+
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+mongoose.connect(process.env.DATABASE_CONNECTION,{ 
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    }).then(()=>{
+        console.log("DB CONNECTED") 
+    }).catch((error)=>console.log(error.message))
+
+    
+
+app.get('/',(req,res)=>{
+    res.send("WELCOME TO HACKSCRIPT BACKEND SERVICE,updated(21/12/22)")
+  })
+  
+
+const port = process.env.PORT || 8000;
+    app.listen(port,()=>{
+        console.log(`APP IS RUNNING AND AT ${port}`);
+    })
