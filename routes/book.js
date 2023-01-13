26cc65a76c6c79d23213820d47d535adc1594628
Atish Yadav <88705566+atishcomp@users.noreply.github.com>
2023-01-13 21:03:13 +0530

    Update book.js

--- routes/book.js
+++ routes/book.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {createBook,getAllBooks,updateBook,deleteBook,getBook} = require("../controllers/book.js")
 
-
+//all routes
 router.post("/book/create",createBook)
 router.get("/book/allbooks",getAllBooks)
 router.get("/book/getbook",getBook)
